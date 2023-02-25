cef8c744aac15717eebe18cafbbe380504097cee
CAshutosh <123365436+CAshutosh@users.noreply.github.com>
2023-02-25 14:41:31 +0530

    Update ec2-instance.tf

--- terraform-manifests/ec2-instance.tf
+++ terraform-manifests/ec2-instance.tf
@@ -69,17 +69,3 @@ module "nexus" {
   vpc_security_group_ids = [module.ec2-security-group.security_group_id]
   tags = local.common_tags
 }
-
-module "sonarqube" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "4.3.0"
-  # insert the required variables here
-  name                   = var.instance_name[5]
-  ami                    = data.aws_ami.amzlinux2.id
-  instance_type          = var.instance_type[2]
-  key_name = var.instance_keypair
-  user_data = file("./scripts/SonarqubeInstallation.sh")
-  subnet_id              = module.vpc.public_subnets[0]
-  vpc_security_group_ids = [module.ec2-security-group.security_group_id]
-  tags = local.common_tags
-}
